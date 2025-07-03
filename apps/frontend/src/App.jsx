6c53849beb0b8b4d055a4d6ced81b9df809c3919
shrikaran ks <shrikaranksise2024@gmail.com>
2025-07-03 07:30:20 +0530

    Dasboard setup

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Authpage from "./pages/Authpage"
+import Dasboard from "./pages/Dasboard"
 
 function App() {
   return (
     <div className="h-screen w-screen">
       <Routes>
         <Route path="/" element={<Authpage/>}/>
+        <Route path="/dashboard" element={<Dasboard/>}/>
       </Routes>
     </div>
   )
---
+++ apps/frontend/src/pages/Dasboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dasboard() {
+  return (
+    <div className=''>
+        welcome bro this is the dashboard
+    </div>
+  )
+}
+
+export default Dasboard
